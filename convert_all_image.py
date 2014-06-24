bff3f8dcecdea485458401698525fcf0ee5e2862
Jonathan Grizou <jonathan.grizou@inria.fr>
2014-06-24 16:26:15 +0200

    add a script to change all visuals dpi and converted them all in 50 dpi for faster compilation while working on thesis

---
+++ convert_all_image.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import os
+import sys
+import subprocess
+
+dpi = sys.argv[1]
+
+hereFolder = os.path.dirname(os.path.realpath(__file__))
+visualsFolder = os.path.join(hereFolder, "visuals")
+visualsForThesisFolder = os.path.join(hereFolder, "visuals"+str(dpi))
+
+if not os.path.exists(visualsForThesisFolder):
+    os.makedirs(visualsForThesisFolder)
+
+for path, subdirs, files in os.walk(visualsFolder):
+   for filename in files:
+   		extension = os.path.splitext(filename)[1]
+   		if extension == ".png":
+
+			cmd = "convert "
+			cmd += os.path.join(path, filename)
+			cmd += " -units PixelsPerInch -resample "
+			cmd += str(dpi)
+			cmd += " "
+
+   			newFolder =  path.replace(visualsFolder, visualsForThesisFolder)
+			if not os.path.exists(newFolder):
+			    os.makedirs(newFolder)
+
+			newFilename = os.path.join(newFolder, filename)
+			cmd += newFilename
+
+			subprocess.call(cmd, shell=True)
