d50fa645b91b912c962147a6ec49d0e2354b8eb3
Jonathan Grizou <jonathan.grizou@inria.fr>
2014-06-26 10:40:10 +0200

    modifying convert script to show status

--- convert_all_image.py
+++ convert_all_image.py
@@ -13,22 +13,31 @@ visualsForThesisFolder = os.path.join(hereFolder, "visuals"+str(dpi))
 if not os.path.exists(visualsForThesisFolder):
     os.makedirs(visualsForThesisFolder)
 
+cnt = 0
 for path, subdirs, files in os.walk(visualsFolder):
-   for filename in files:
-   		extension = os.path.splitext(filename)[1]
-   		if extension == ".png":
+    for filename in files:
+        extension = os.path.splitext(filename)[1]
+        if extension == ".png":
+            cnt += 1
 
-			cmd = "convert "
-			cmd += os.path.join(path, filename)
-			cmd += " -units PixelsPerInch -resample "
-			cmd += str(dpi)
-			cmd += " "
+            cmd = "convert "
+            cmd += os.path.join(path, filename)
+            cmd += " -units PixelsPerInch -resample "
+            cmd += str(dpi)
+            cmd += " "
 
-   			newFolder =  path.replace(visualsFolder, visualsForThesisFolder)
-			if not os.path.exists(newFolder):
-			    os.makedirs(newFolder)
+            newFolder =  path.replace(visualsFolder, visualsForThesisFolder)
+            if not os.path.exists(newFolder):
+                os.makedirs(newFolder)
 
-			newFilename = os.path.join(newFolder, filename)
-			cmd += newFilename
+            newFilename = os.path.join(newFolder, filename)
+            cmd += newFilename
 
-			subprocess.call(cmd, shell=True)
+            dispStr = str(cnt) + ": converting " + filename + " ..."
+            print dispStr
+            
+            subprocess.call(cmd, shell=True)
+
+
+
+
